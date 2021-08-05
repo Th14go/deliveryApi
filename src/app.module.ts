bbe5007c552f877f014e7aca8e8f7edc7650d8e3
Thiago Lopes <thiagoolopezz@gmail.com>
2021-08-04 23:42:51 -0300

    categories recursos
    erro em post

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryModule } from './categories/category.module';
+
 @Module({
-  imports: [],
+  imports: [
+    CategoryModule,
+    MongooseModule.forRoot('mongodb+srv://delivery:th14go@L21@cluster0.ybtur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/categories/shared/category.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './category';
+
+@Injectable()
+export class CategoryService {
+
+    constructor(
+        @InjectModel('Category') private readonly categoryModel: Model<Category>) { }
+
+
+    async getAll() {
+        return await this.categoryModel.find().exec();
+
+    }
+
+    async getbyId(id: string) {
+        return await this.categoryModel.findById(id).exec();
+    }
+
+    async create(category: Category) {
+        const createdCategory = new this.categoryModel(category);
+        return await createdCategory.save();
+    }
+
+    async update(id: string, category: Category) {
+        return await this.categoryModel.findByIdAndUpdate(id, category);
+    }
+
+    async delete(id: string) {
+        return await this.categoryModel.deleteOne({_id: id}).exec();
+    }
+}
